e7215906d44af8841ab66ddc8fb8490568e2c7ca
Dipesh Jaiswal <dipeshjaiswal140@gmail.com>
2021-03-05 16:23:05 +0530

    Add a Bottom Navigation

---
+++ src/layouts/BaseLayout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import BottomNav from '../components/BottomNav'
+
+const BaseLayout = () => {
+    return (
+        <div>
+            <BottomNav />
+        </div>
+    )
+}
+
+export default BaseLayout
